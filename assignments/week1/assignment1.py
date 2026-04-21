68bad02e9d12fc75f46389fd9e64eb632c7971d1
xeniawaal <xenia.waal@stud.leuphana.de>
2026-04-21 09:47:36 +0200

    A little scratch of a dachshund with the comparison of my old real dog
    I did a small scratch in python of a dachshund who looks very similar to my childhood dog who was half of a dachshund/ sausage dog

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -1,1 +1,23 @@
+# techBasics1_Xenia_Waal 
+ 
+## My name is Xenia Waal and I am a 2nd semester Digital Media student 
+
+# This ASCII art shows a dachshund (sausage dog)
+# I created it because it looks similar to my childhood dog who is sadly not alive anymore
+# (and because i looove dogs!) 
+
+```python
+print("   /^-----^\\") 
+print("  V  o o  V") 
+print("   |  Y  |") 
+print("    \\ Q /") 
+print("    / - \\") 
+print("   |    \\") 
+print("   |     \\     )") 
+print("   || (___\\====")
+```
+
+
+# the next picture is him 
+![Screenshot](Bildschirmfoto%202026-04-20%20um%2012.22.11.png) 
 
